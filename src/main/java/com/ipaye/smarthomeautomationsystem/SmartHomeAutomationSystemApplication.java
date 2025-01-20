7f08ffbb6924e360fea00c2975998fb2d4fb3443
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-20 18:59:53 +0100

    init commit

---
+++ src/main/java/com/ipaye/smarthomeautomationsystem/SmartHomeAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ipaye.smarthomeautomationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeAutomationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeAutomationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipaye/smarthomeautomationsystem/SmartHomeAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipaye.smarthomeautomationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeAutomationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
