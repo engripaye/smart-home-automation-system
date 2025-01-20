965431fe7c94759979ed73c3c596c4d636fcb320
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-20 19:26:23 +0100

    init commit

--- src/main/java/com/ipaye/smarthomeautomationsystem/Entity/Device.java
+++ src/main/java/com/ipaye/smarthomeautomationsystem/Entity/Device.java
@@ -1,4 +1,60 @@
 package com.ipaye.smarthomeautomationsystem.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Device {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private String status;
+    private String location;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type=type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status=status;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location=location;
+    }
 }
